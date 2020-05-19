dd9bccea0dc8a459d57b7eb0d86935b16211e5b7
Mark Pictor <mark.pictor@genesys.com>
2020-05-19 17:13:14 -0500

    missing copyrights
    Signed-off-by: Mark Pictor <mark.pictor@genesys.com>

--- gopath/src/gprovision/cmd/util/pbLogServer/srv.go
+++ gopath/src/gprovision/cmd/util/pbLogServer/srv.go
@@ -1,3 +1,10 @@
+// Copyright (C) 2015-2020 the Gprovision Authors. All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
 package main
 
 import (
