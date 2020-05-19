cfc0429c5792e2f19fb76b604330184c4af47bf4
Mark Pictor <mark.pictor@genesys.com>
2020-05-19 14:41:25 -0500

    remove unused files
    Signed-off-by: Mark Pictor <mark.pictor@genesys.com>

--- gopath/src/gprovision/pkg/hw/block/const.go
+++
@@ -1,9 +0,0 @@
-package block
-
-//constants
-
-const (
-	KB = 1024
-	MB = 1024 * 1024
-	GB = 1024 * 1024 * 1024
-)
--- gopath/src/gprovision/pkg/oss/frd/frd_integ.go
+++
@@ -1,9 +0,0 @@
-// +build !release
-
-package frd
-
-//This file contains functions used in integ tests.
-
-func IntegPersist(dir string, data Frjson) error {
-	return persist(dir, data)
-}
